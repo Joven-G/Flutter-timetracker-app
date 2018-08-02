218d0096374e7c2d47ef6ab68c6a50177d589335
ballanse <devblackholes@gmail.com>
2018-08-02 16:02:21 +0800

    Update HomePage layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,21 +8,21 @@ class HomePage extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: ListView(
-            children: <Widget>[
-              SizedBox(height: 18.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 36.0),
-                child: Center(
-                  child: Text('00:00:00', style: TextStyle(fontSize: 36.0)),
-                ),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 18.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36.0),
+              child: Center(
+                child: Text('00:00:00', style: TextStyle(fontSize: 36.0)),
               ),
-              SizedBox(height: 16.0),
-              GraphCard(),
-              SizedBox(height: 16.0),
-              Row(
+            ),
+            SizedBox(height: 16.0),
+            GraphCard(),
+            SizedBox(height: 16.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Row(
                 children: <Widget>[
                   Expanded(
                     child: Text('Recent Weeks', style: textTheme.title),
@@ -35,31 +35,59 @@ class HomePage extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(height: 48.0),
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  AButton(
-                    isRaised: true,
-                    label: "TIME IN",
-                    onPressed: () {
-                      print("Time In...");
-                    },
-                  ),
-                  AButton(
-                    isRaised: false,
-                    label: "TIME OUT",
-                    onPressed: () {
-                      print("Time Out");
-                    },
-                  ),
-                ],
+            ),
+            SizedBox(height: 16.0),
+            Container(
+              height: 160.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: _buildCards(5),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 16.0),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                AButton(
+                  isRaised: true,
+                  label: "TIME IN",
+                  onPressed: () {
+                    print("Time In...");
+                  },
+                ),
+                AButton(
+                  isRaised: false,
+                  label: "TIME OUT",
+                  onPressed: () {
+                    print("Time Out");
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  List<Widget> _buildCards (int count) {
+    final cardList = <Widget>[];
+
+    for (int i = 0; i < count; i++) {
+      cardList.add(
+        Container(
+          width: 200.0,
+          child: ACard(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(i.toString()),
+            ),
+          ),
+        )
+      );
+    }
+
+    return cardList.toList();
   }
 }
 
@@ -112,22 +140,24 @@ class ChartColumn extends StatelessWidget {
 class GraphCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ACard(
-      color: Colors.white,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            // TODO: Add Bars
-            ChartColumn(label: 'MON', height: .8),
-            ChartColumn(label: 'TUE', height: .65),
-            ChartColumn(label: 'WED', height: .86),
-            ChartColumn(label: 'THU', height: .43),
-            ChartColumn(label: 'FRI', height: .95),
-          ],
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: ACard(
+        color: Colors.white,
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              // TODO: Add Bars
+              ChartColumn(label: 'MON', height: .8),
+              ChartColumn(label: 'TUE', height: .65),
+              ChartColumn(label: 'WED', height: .86),
+              ChartColumn(label: 'THU', height: .43),
+              ChartColumn(label: 'FRI', height: .95),
+            ],
+          ),
         ),
       ),
     );
